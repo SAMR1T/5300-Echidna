90e281b0008be07145d1b8a865ff0669e787238b
Tong Ding <dingtong@seattleu.edu>
2020-04-24 02:00:03 -0700

    done implementing SlottedPage

--- heap_storage.cpp
+++ heap_storage.cpp
@@ -10,7 +10,7 @@
  */
 
 #include "heap_storage.h"
-#include "db_cxx.h"
+// #include "db_cxx.h"
 #include <cstring>
 
 using namespace std;
@@ -70,26 +70,21 @@ RecordID SlottedPage::add(const Dbt *data) {
 
 /**
  * Get a record from the block given record id
- * Return NULL if it has been deleted
+ * Return nullptr if it has been deleted
  * @param record_id the id given for a record
  * @return the record for the given id
  */
 Dbt* SlottedPage::get(RecordID record_id) {
-    // get location and length of the record
+    // get offset and length of the record
     u16 loc, size;
-    this->get_header(size, loc, record_id);
+    get_header(size, loc, record_id);
 
     // check if it's tombstone of deleted record
     if (loc == 0)
         return nullptr;
     
-    // get data content
-    char byte_string[size];
-    memcpy(byte_string, this->address(loc), size);
-
-    // Dbt(void *data, size_t size); 
-    Dbt *data = new Dbt(byte_string, size);
-
+    // form record to return
+    Dbt *data = new Dbt(this->address(loc), size);
     return data;
 }
 
@@ -100,77 +95,81 @@ Dbt* SlottedPage::get(RecordID record_id) {
  * @exception DbBlockNoRoomError if no room for record update
  */ 
 void SlottedPage::put(RecordID record_id, const Dbt &data) {
-    // get location and size info
+    // get offset and size info
     u16 loc, size, new_size;
-    this->get_header(size, loc, record_id);
+    get_header(size, loc, record_id);
     new_size = data.get_size();
 
     // check if update size 
     if (new_size > size) {
 
         // check if block room enough for update
-        if (!this->has_room(new_size - size))
+        if (!has_room(new_size - size))
             throw DbBlockNoRoomError("block room not enough for record update");
 
         // slide and update data
-        this->slide(loc, loc - new_size + size);
+        slide(loc, loc - new_size + size);
         memcpy(this->address(loc - new_size + size), data.get_data(), new_size);
 
     } else {
         // update data then slide
         memcpy(this->address(loc), data.get_data(), new_size);
-        this->slide(loc + new_size, loc + size);
+        slide(loc + new_size, loc + size);
     }
-    
     // update header
-    this->get_header(size, loc, record_id);
-    this->put_header(record_id, new_size, loc);
+    get_header(size, loc, record_id);
+    put_header(record_id, new_size, loc);
 };
 
 /**
  * Mark the given record_id as deleted by changing its size to 
- * zero and its location to 0. Compact the rest of the data in 
+ * zero and its offset to 0. Compact the rest of the data in 
  * the block. But keep the record ids the same for everyone.
  * @param record_id the id given for a record
  */ 
 void SlottedPage::del(RecordID record_id) {
-    // get location and size info
+    // get offset and size info
     u16 loc, size;
-    this->get_header(size, loc, record_id);
+    get_header(size, loc, record_id);
 
     // update header and slide
-    this->put_header(record_id, 0, 0);
-    this->slide(loc, loc + size);
+    put_header(record_id, 0, 0);
+    slide(loc, loc + size);
 };
 
 /**
  * Get the sequence of all non-deleted record ids
  * @return ids of all non-deleted records
  */ 
-RecordIDs * SlottedPage::ids(void) {return nullptr;}
-// RecordIDs * SlottedPage::ids(void) {
-//     u16 loc, size, i = 0;
-//     RecordIDs ids[this->num_records];
+RecordIDs* SlottedPage::ids(void) {
+    u16 loc, size, i = 1;
+    RecordIDs *ids = new vector<RecordID>();
 
-//     while (i < this->num_records) {
-//         this->get_header(size, loc, i + 1);
-//         if (size != 0)
-//             ids[i++] = i + 1;
-//     }
-//     return ids;
-// }
+    while (i < this->num_records + 1) {
+        get_header(size, loc, i);
+
+        // non-deleted record has size > 0
+        if (size > 0)
+            ids->push_back((RecordID)i);
+        ++i;
+    }
+    return ids;
+}
 
 /**
- * Get the size and offset for given record_id. 
+ * Get the size and offset for given record_id
  * For record_id of zero, it is the block header
- * @param &size
- * @param &loc
- * @param id
+ * @param &size addr to hold size
+ * @param &loc addr to hold offset
+ * @param id given id of a record
  */ 
 void SlottedPage::get_header(u16 &size, u16 &loc, RecordID id) {
+    // check if from start
     if (id == 0) {
         size = 0;
         loc = 0;
+    
+    // get size and offset
     } else {
         size = this->get_n(4 * id);
         loc = this->get_n(4 * id + 2);
@@ -188,16 +187,61 @@ void SlottedPage::put_header(RecordID id, u16 size, u16 loc) {
     // if call put_header(), use default params 
     if (id == 0) {  
         size = this->num_records;   // (0, 0, 0) ???
-        loc = this->end_free;
+        loc = this->end_free;       // (0, 0, 0) ???
     }
     // put header with size and offset
     put_n(4 * id, size);
     put_n(4 * id + 2, loc);
 }
 
-bool SlottedPage::has_room(u16 size) {return false;};
+/**
+ * Calculate if room is enough to store a record with given size
+ * @param size the size of record to store
+ * @return True if enough room, otherwise False
+ */
+bool SlottedPage::has_room(u16 size) {
+    u16 available = this->end_free - (this->num_records + 2) * 4;
+    return size <= available;
+}
 
-void SlottedPage::slide(u16 start, u16 end) {};
+/**
+ * Remove data from offset [start, end) by sliding data that 
+ * is to the left of start to the right, if start < end.  
+ * Make room for extra data from end to start by sliding data 
+ * that is to the left of start to the left, If start > end.
+ * Also fix up any record headers whose data has slid. Assumes 
+ * there is enough room if it is a left shift (end < start).
+ * @param start offset start for sliding data
+ * @param end offset end for sliding data
+ */ 
+void SlottedPage::slide(u16 start, u16 end) {
+    // calc shift
+    u16 shift = end - start;
+    if (shift == 0)
+        return;
+
+    // slide
+    u16 loc = this->end_free + 1;
+    memcpy(this->address(loc + shift), this->address(loc), start - loc);
+
+    // update header
+    u16 size;
+    RecordIDs *ids = this->ids();
+    RecordID *id = ids->data();
+
+    for (uint i = 0; i < ids->size(); ++i) {
+        get_header(size, loc, *id);
+        if (loc <= start) {
+            loc += shift;
+            put_header(*id, size, loc);
+        }
+        id++;
+    }
+
+    // update free end offset
+    this->end_free += shift;
+    this->put_header();
+};
 
 /** 
  * Get a 2-byte integer at given offset in block
