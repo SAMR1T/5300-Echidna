cc56f492e0729ab0471cab5dfb78ba9fbda7e80b
patricianaomi <patricianaomi99@gmail.com>
2020-05-06 20:31:02 -0700

    given instructor files

--- sql5300.cpp
+++ sql5300.cpp
@@ -1,265 +1,89 @@
 /**
- * @file sql5300.cpp main entry for the sql5300 relational db manager for spsc5300 course
- * @author  Yibo Sheng, Tong(Debby) Ding
- * @version Team Echidna, Sprint Verano
- * @see Seattle U, CPSC 5300, Spring 2020
+ * @file sql5300.cpp - main entry for the relation manager's SQL shell
+ * @author Kevin Lundeen
+ * @see "Seattle University, cpsc4300/5300, summer 2018"
  */
-
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+#include <cstdlib>
+#include <iostream>
+#include <string>
 #include "db_cxx.h"
 #include "SQLParser.h"
-#include "heap_storage.h"
+#include "ParseTreeToString.h"
+#include "SQLExec.h"
 
-
-DbEnv *_DB_ENV;
 using namespace std;
 using namespace hsql;
 
-/**
- * Convert the hyrise ColumnDefinition AST back to equivalent SQL
- * @param col column definition to unparse
- * @return SQL equivalent to *col
+/*
+ * we allocate and initialize the _DB_ENV global
  */
-string convertColToStr(const ColumnDefinition *col) {
-	string res(col->name);
-	switch(col->type) {
-		case ColumnDefinition::INT:
-			res += " INT";
-			break;
-		case ColumnDefinition::DOUBLE:
-			res += " DOUBLE";
-			break;
-		case ColumnDefinition::TEXT:
-			res += " TEXT";
-			break;
-		default:
-			res += " UNKNOWN";
-			break;
-	}
-	return res;
-}
+void initialize_environment(char *envHome);
+
 
 /**
- * Convert the hyrise CreateStatement AST back to equivalent SQL
- * @param stmt statement to unparse
- * @return SQL equivalent to *stmt
+ * Main entry point of the sql5300 program
+ * @args dbenvpath  the path to the BerkeleyDB database environment
  */
-string executeCreate(const CreateStatement *stmt) {
-	string res("CREATE TABLE ");
+int main(int argc, char *argv[]) {
 
-	if (stmt->type != CreateStatement::kTable)
-		return res + "...";
-	if (stmt->ifNotExists)
-		res += "IF NOT EXISTS ";
-	
-	res += string(stmt->tableName) + " (";
+    // Open/create the db environment
+    if (argc != 2) {
+        cerr << "Usage: cpsc5300: dbenvpath" << endl;
+        return EXIT_FAILURE;
+    }
+    initialize_environment(argv[1]);
 
-	// locate the column info in the statement
-	vector<ColumnDefinition*> *colms = stmt->columns;
-
-	// get the name & type of each column
-	for (uint i = 0; i < colms->size(); ++i) {
-		res += convertColToStr(colms->at(i)) + ", ";
-	}
-	res.resize(res.size() - 2);
-	res += ")";
-
-	return res;
-}
-
-/**
- * Convert the hyrise expression item AST back to equivalent SQL
- * @param item item to unparse
- * @return SQL equivalent to *item
- */
-string convertToStr(const Expr* item) {
-	string res = "";
-	switch(item->type) {
-		case kExprStar:
-			res += "*";
-			break;	
-		case kExprColumnRef:
-			// just col name
-			if (item->table == NULL) {
-				res += string(item->name);
-				break;
-			}
-			// table/alias name + col name
-			if (item->alias != NULL) {
-				res += string(item->alias) + "." + string(item->name);
-			} else {
-				res += string(item->table) + "." + string(item->name);
-			}
-			break;
-		case kExprOperator:
-			res += convertToStr(item->expr) + " ";
-			res += item->opChar;
-			res += " " +  convertToStr(item->expr2);
-			break;
-		case kExprLiteralFloat:
-			res += to_string(item->fval);
-			break;
-		case kExprLiteralInt:
-			res += to_string(item->ival);
-			break;
-		default:
-			res += "Not Implemented";
-			break;
-	}
-	return res;
-}
-
-/**
- * Convert the hyrise JoinDefinition AST back to equivalent SQL
- * @param join join to unparse
- * @return SQL equivalent to *join
- */
-string convertJoin(const JoinDefinition *join) {
-	switch(join->type) {
-		case kJoinLeft:
-			return " LEFT JOIN ";
-		case kJoinRight:
-			return " RIGHT JOIN ";
-		default:
-			return " JOIN ";
-	}
-}
-
-/**
- * Convert the hyrise TableRef AST back to equivalent SQL
- * @param table table to unparse
- * @return SQL equivalent to *table
- */
-string convertTableRef(const TableRef *table) {
-	string res = "";
-	switch(table->type) {
-		case kTableName:
-			res += string(table->name);
-			if (table->alias != NULL) {
-				res += " AS " + string(table->alias);
-			}
-			break;
-		case kTableJoin:
-			// JoinDefinition* join = table->join;
-			res += convertTableRef(table->join->left) + convertJoin(table->join);
-			res += convertTableRef(table->join->right);
-			res += " ON " + convertToStr(table->join->condition);
-			break;  
-		case kTableCrossProduct:
-			// vector<TableRef*>* tables = table->list;
-			for (TableRef *tab : *table->list) {
-				res += convertTableRef(tab) + ", ";
-			}
-			res.resize(res.size() - 2);
-			break;
-		default:
-			res += "Not Implemented";
-			break;
-	}
-	return res;
-}
-
-/**
- * Convert the hyrise SelectStatement AST back to equivalent SQL
- * @param stmt statement to unparse
- * @return SQL equivalent to *stmt
- */
-string executeSelect(const SelectStatement *stmt) {
-	string res("SELECT ");
-
-	// get the name of cols in the select list
-	vector<Expr*>* selectList = stmt->selectList;
-
-	for (uint i = 0; i < selectList->size(); ++i) {
-		// note: may have "*" instead of cols
-		res += convertToStr(selectList->at(i)) + ", ";
-	}
-	res.resize(res.size() - 2);
-
-	// get the name of table to select cols from
-	TableRef* table = stmt->fromTable;
-
-	res += " FROM " + convertTableRef(table);
-
-	// get the where clause if exists
-	if (stmt->whereClause != NULL)
-		res += " WHERE " + convertToStr(stmt->whereClause);
-	return res;
-}
-
-/**
- * Convert the hyrise SQLStatement AST back to equivalent SQL
- * @param stmt statement to unparse
- * @return SQL equivalent to *stmt
- */ 
-string execute(const SQLStatement *stmt) {
-	switch(stmt->type()) {
-		case kStmtSelect:
-			return executeSelect((const SelectStatement *)stmt);
-		case kStmtCreate:
-			return executeCreate((const CreateStatement *)stmt);
-		case kStmtInsert:
-			return "INSERT ..." ;
-		default:
-			return "Not implemented";
-	}
-}
-
-/**
- * Validate and convert the SQL statements entered by user
- * @param argc number of user input
- * @param argv[] the content of user input
- */ 
-int main(int argc, char* argv[]) {
-
-	if (argc != 2) {
-		cerr << "Usage: ./sql5300 dbenvpath" <<endl;
-		return EXIT_FAILURE;
-	}
-
-	char* envHome = argv[1];
-
-	// create BerkeleyDB environment
-	DbEnv env(0U);
-	env.set_message_stream(&std::cout);
-	env.set_error_stream(&std::cerr);
-	env.open(envHome, DB_CREATE | DB_INIT_MPOOL, 0);
-
-	cout << "\nWelcome! Enter a SQL statement or \"quit\" to exit.\n" <<endl;
-	
-	// create user-input loop
-	while (true) {
-		cout << "SQL> ";
-		string query;
-		getline(cin, query);
-		if (query.length() == 0) {
-			continue;
-		}
-		// exit
-		if (query == "quit") {
-			break;
-		}
-		if (query == "test") {
+    // Enter the SQL shell loop
+    while (true) {
+        cout << "SQL> ";
+        string query;
+        getline(cin, query);
+        if (query.length() == 0)
+            continue;  // blank line -- just skip
+        if (query == "quit")
+            break;  // only way to get out
+        if (query == "test") {
             cout << "test_heap_storage: " << (test_heap_storage() ? "ok" : "failed") << endl;
             continue;
         }
 
-		// parse query to a parse tree
-		SQLParserResult* result = SQLParser::parseSQLString(query);
+        // parse and execute
+        SQLParserResult *parse = SQLParser::parseSQLString(query);
+        if (!parse->isValid()) {
+            cout << "invalid SQL: " << query << endl;
+            cout << parse->errorMsg() << endl;
+        } else {
+            for (uint i = 0; i < parse->size(); ++i) {
+                const SQLStatement *statement = parse->getStatement(i);
+                try {
+                    cout << ParseTreeToString::statement(statement) << endl;
+                    QueryResult *result = SQLExec::execute(statement);
+                    cout << *result << endl;
+                    delete result;
+                } catch (SQLExecError &e) {
+                    cout << "Error: " << e.what() << endl;
+                }
+            }
+        }
+        delete parse;
+    }
+    return EXIT_SUCCESS;
+}
 
-		// check if parse tree is valid
-		if (result->isValid()) {
-			for (uint i = 0; i < result->size(); ++i) {
-				cout << "---> " << execute(result->getStatement(i)) << endl;
-			}
-		} else {
-			cout << " X Invalid SQL statement." <<endl;
-		}
+DbEnv *_DB_ENV;
 
-		delete result;
-	}
+void initialize_environment(char *envHome) {
+    cout << "(sql5300: running with database environment at " << envHome << ")" << endl;
 
-	return EXIT_SUCCESS;
+    DbEnv *env = new DbEnv(0U);
+    env->set_message_stream(&cout);
+    env->set_error_stream(&cerr);
+    try {
+        env->open(envHome, DB_CREATE | DB_INIT_MPOOL, 0);
+    } catch (DbException &exc) {
+        cerr << "(sql5300: " << exc.what() << ")" << endl;
+        exit(1);
+    }
+    _DB_ENV = env;
+    initialize_schema_tables();
 }
