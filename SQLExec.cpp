8a57b89922b6db4bce64d0d17f72ee0e13b7ab18
SAMR1T <samrit.dhesi@hotmail.com>
2020-05-07 23:49:19 -0700

    updated show, show_tables, create, and destructor

--- SQLExec.cpp
+++ SQLExec.cpp
@@ -13,7 +13,8 @@ using namespace hsql;
 Tables *SQLExec::tables = nullptr;
 
 // make query result be printable
-ostream &operator<<(ostream &out, const QueryResult &qres) {
+ostream &operator<<(ostream &out, const QueryResult &qres) 
+{
     if (qres.column_names != nullptr) {
         for (auto const &column_name: *qres.column_names)
             out << column_name << " ";
@@ -43,13 +44,20 @@ ostream &operator<<(ostream &out, const QueryResult &qres) {
     return out;
 }
 
-QueryResult::~QueryResult() {
-    delete Tables;  //FIX ME
+// FIX ME --> Not done, just an idea
+QueryResult::~QueryResult()
+{
+    delete tables;
+    delete column_names;
+    delete column_attributes;
+    for(ValueDict *row: *rows)
+            delete row;
+    delete rows;
 }
 
 
-QueryResult *SQLExec::execute(const SQLStatement *statement) {
-
+QueryResult *SQLExec::execute(const SQLStatement *statement)
+{
     if(SQLExec::tables == nullptr)
         SQLExec::tables = new Tables();
 
@@ -69,17 +77,25 @@ QueryResult *SQLExec::execute(const SQLStatement *statement) {
     }
 }
 
-void
-SQLExec::column_definition(const ColumnDefinition *col, Identifier &column_name, ColumnAttribute &column_attribute) {
+void SQLExec::column_definition(const ColumnDefinition *col, Identifier &column_name, ColumnAttribute &column_attribute)
+{
     throw SQLExecError("not implemented");  // FIXME
 }
 
-QueryResult *SQLExec::create(const CreateStatement *statement) {
-    return new QueryResult("not implemented"); // FIXME
+QueryResult *SQLExec::create(const CreateStatement *statement)
+{
+    swtich(statement->type)
+    {
+        case CreateStatement::kTable:
+            return create_table(statement);
+        default:
+            return new QueryResult("Only CREATE TABLE is supported");
+    }
 }
 
-// DROP ...
-QueryResult *SQLExec::drop(const DropStatement *statement) { //FIXME
+// FIX ME --> Not done, just based on his instructions but won't work?
+QueryResult *SQLExec::drop(const DropStatement *statement) 
+{
 
     if (statement->type != DropStatement::kTable)
         throw SQLExecError("Unrecognized DROP type");
@@ -92,15 +108,52 @@ QueryResult *SQLExec::drop(const DropStatement *statement) { //FIXME
     return new QueryResult(std::string("dropped ") + table_name); 
 }
 
-QueryResult *SQLExec::show(const ShowStatement *statement) {
-    return new QueryResult("not implemented"); // FIXME
+QueryResult *SQLExec::show(const ShowStatement *statement) 
+{
+    switch(statement->type)
+    {
+        case ShowStatement::kTables:
+            return show_tables();
+        case ShowStatement::kColumns:
+            return show_columns(statement);
+        default:
+            throw SQLExecError(" not implemented, only SHOW TABLES and SHOW COLUMNS is supported.");
+    } 
+    return NULL; 
 }
 
-QueryResult *SQLExec::show_tables() {
-    return new QueryResult("not implemented"); // FIXME
+QueryResult *SQLExec::show_tables()
+{
+    ColumnNames *resultsColNames = new ColumnNames();
+    ColumnAttributes *resultsColAttribs = new ColumnAttributes();
+
+    // We need to get the column names and it's attributes
+    tables->get_columns(Tables::TABLE_NAME, *resultsColNames, *resultsColAttribs);
+
+    // Handles to all table entries
+    Handles *handles = tables->select();
+
+    // Create a vector to hold all the values
+    ValueDicts *rows = new ValueDicts();
+
+    // Go through the handles and add them to the ValueDict
+    for(Handle handle : *handles)
+    {
+        ValueDict *row = tables->project(handle, resultsColNames);
+        if(row->at("table_name") != Value("_tables") && row->at("table_name") != Value("_columns")) 
+            rows->push_back(row);
+    }
+    delete handles;
+
+    // Use QueryResult with all results from queries (return cn, ca, rows, message)
+    string message = " Succesfully got " + to_string(rows->size()) + " rows";
+    return new QueryResult(resultsColNames, resultsColAttribs, rows, message);
 }
 
-QueryResult *SQLExec::show_columns(const ShowStatement *statement) {
+QueryResult *SQLExec::show_columns(const ShowStatement *statement) 
+{
+    // i think this will be similar to show table
+    // except we focus on column...
     return new QueryResult("not implemented"); // FIXME
 }
 
